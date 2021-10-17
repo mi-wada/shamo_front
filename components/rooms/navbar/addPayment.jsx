1318c1efa9e689958d03d03b95e9ae9bdde50155
mi-wada <mtak.wd@gmail.com>
2021-10-17 20:34:17 +0900

    fix behavior of modals

--- components/rooms/navbar/addPayment.jsx
+++ components/rooms/navbar/addPayment.jsx
@@ -16,6 +16,7 @@ const modalStyle = {
     left: '50%',
     right: 'auto',
     bottom: 'auto',
+    width: '90%',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     borderRadius: '1rem',
