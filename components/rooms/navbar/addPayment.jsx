c81c90d40f0e64755040c61763b137887a772b34
mi-wada <mtak.wd@gmail.com>
2021-11-15 16:38:54 +0900

    fix key name

--- components/rooms/navbar/addPayment.jsx
+++ components/rooms/navbar/addPayment.jsx
@@ -69,8 +69,8 @@ export default function AddPayment({ roomId, users }) {
               <label className="mr-5 text-gray-700 font-bold inline-block mb-2" for="name">Who<span className="text-xs text-red-600">*</span></label>
               <select id="who" className="border bg-gray-100 py-2 px-4 w-72 outline-none focus:ring-2 focus:ring-gray-600 rounded">
                 {(users || []).map(user => (
-                  <option value={user.Id}>
-                    {user.Name}
+                  <option value={user.id}>
+                    {user.name}
                   </option>
                 ))}
               </select>
